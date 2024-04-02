acb3bc433af7f7e5b289de79368d6e2ec70ba120
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-03 00:15:05 +0530

    node js

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,12 @@
+//var generateName = require('sillyname');
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`my name is ${sillyName}`);
+
+// const superheroes = require('superheroes');
+
+import superheroes from 'superheroes';
+
+console.log(`I am a superhero name is ${superheroes.random()}`);
