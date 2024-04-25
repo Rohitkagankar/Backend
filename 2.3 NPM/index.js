11c8de2838930ccc5bcfa11d6683078f734ce8c1
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-25 17:50:25 +0530

    using node modules

--- 2.3 NPM/index.js
+++ 2.3 NPM/index.js
@@ -4,9 +4,11 @@ import generateName from "sillyname";
 var sillyName = generateName();
 
 console.log(`my name is ${sillyName}`);
+console.log(`hello I am ${sillyName} , I likes Mangoes.`)
 
 // const superheroes = require('superheroes');
 
 import superheroes from 'superheroes';
 
 console.log(`I am a superhero name is ${superheroes.random()}`);
+console.log(` the name of my favorite superheros is ${superheroes.random()}`);
