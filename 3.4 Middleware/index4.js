8186cccdebe5cc4f03e30de04c278843592c3cf9
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-26 18:39:22 +0530

    multipage

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,31 +1,48 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
+import {dirname} from "path";
 import { fileURLToPath } from "url";
-var bandName="";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const __dirname=dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+var bname="";
+var Name="";
+var age;
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-
-function bandNameGenerator(req,res, next){
+function NamePetGen(req,res,next){
   console.log(req.body);
-  bandName=req.body["street"]+req.body["pet"];
+  bname=req.body["street"]+req.body["pet"];
   next();
 }
+app.use(NamePetGen);
 
+function nameGen(req,res,next){
+  console.log(req.body);
+  Name=req.body["name"];
+  age=req.body["age"];
+  next();
+}
+app.use(nameGen);
 
-app.use(bandNameGenerator);
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.sendFile(__dirname+"/public/index.html");
-})
+});
 
 app.post("/submit",(req,res)=>{
-  res.send(`<h1>Your band Name is </h1><h2>${bandName}</h2>`);
+  res.send(`<h1>Your name is ${bname}</h1>`);
+})
+
+app.get("/registerpage",(req,res)=>{
+  res.sendFile(__dirname+"/public/page.html");
+})
+
+app.post("/register",(req,res)=>{
+  res.send(`<h1>Your name is ${Name}</h1><br><h2>Age : ${age}</h2>`)
 })
 
 app.listen(port, () => {
