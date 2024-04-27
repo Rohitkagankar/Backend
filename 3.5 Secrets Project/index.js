dfaec5db252de78068c203f930955d8a3af1c988
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-04-27 23:51:54 +0530

    completed

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,14 +1,40 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
 import express from "express";
 import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname=dirname(fileURLToPath(import.meta.url));
+
+const port=3000;
+var AuthorisedUser=false;
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+function checkPass(req,res,next){
+  const password=req.body["password"];
+  if(password==="I love codding"){
+    AuthorisedUser=true;
+  }
+  next();
+
+}
+app.use(checkPass);
+
 
 app.get("/",(req,res)=>{
-    
+  res.sendFile(__dirname+"/public/index.html");
 })
+
+app.post("/check",(req,res)=>{
+  if(AuthorisedUser){
+    res.sendFile(__dirname+"/public/secret.html");
+  } else{
+    res.sendFile(__dirname+"/public/index.html");
+  }
+})
+
+app.listen(port,()=>{
+  console.log(`app is listning on port ${port}`);
+})
+
+
